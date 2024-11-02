00b759d6fcfb770379d63490cdd7c11230259c83
Eduardo Longato <edulongato@yahoo.com.br>
2024-11-02 18:00:38 -0300

    Interfaces repositories criadas.

---
+++ src/main/java/com/descomplica/aula/repository/InstrutorRepository.java
@@ -0,0 +1,7 @@
+package com.descomplica.aula.repository;
+
+import com.descomplica.aula.entity.Instrutor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InstrutorRepository extends JpaRepository<Instrutor, Long> {
+}
